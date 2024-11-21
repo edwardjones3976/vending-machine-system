b0b161f133a81957f799f20dba01f29470405573
edwardjones3976 <145315321+edwardjones3976@users.noreply.github.com>
2024-11-20 23:59:57 -0800

    SalesData.h

--- SalesData.h
+++ SalesData.h
@@ -1,16 +1,22 @@
 #pragma once
+#ifndef SALESDATA_H
+#define SALESDATA_H
+
 #include "Transaction.h"
 #include <vector>
 
 class SalesData {
 private:
     float totalSales;
-    std::vector<Transaction*> salesRecords;
+    std::vector<Transaction> salesRecords;
 
 public:
     SalesData();
-    float getDailySales();
-    float getSalesByProduct(int productID);
-    std::vector<Transaction*> generateSalesReport();
-    void recordTransactionData(Transaction* transaction);
+    float getDailySales() const;
+    float getSalesByProduct(int productID) const;
+    std::vector<Transaction> generateSalesReport() const;
+    void addTransaction(const Transaction& transaction);
+    void displaySalesData() const;
 };
+
+#endif // SALESDATA_H
