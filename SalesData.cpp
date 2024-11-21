ab907317e16c2bc2e06924db9614fcede5b2b5b7
edwardjones3976 <145315321+edwardjones3976@users.noreply.github.com>
2024-11-20 23:58:32 -0800

    SalesData.cpp

--- SalesData.cpp
+++ SalesData.cpp
@@ -1,7 +1,39 @@
 #include "SalesData.h"
 #include "Transaction.h"
+#include <iostream>
 
-class SalesData {
-public:
-    SalesData();
-};
+using namespace std;
+
+SalesData::SalesData() : totalSales(0.0f) {}
+
+float SalesData::getDailySales() const {
+    return totalSales;
+}
+
+float SalesData::getSalesByProduct(int productID) const {
+    float productSales = 0.0f;
+    for (const auto& transaction : salesRecords) {
+        if (transaction.getProductID() == productID) {
+            productSales += transaction.getAmountPaid();
+        }
+    }
+    return productSales;
+}
+
+vector<Transaction> SalesData::generateSalesReport() const {
+    return salesRecords;
+}
+
+void SalesData::addTransaction(const Transaction& transaction) {
+    salesRecords.push_back(transaction);
+    totalSales += transaction.getAmountPaid();
+}
+
+void SalesData::displaySalesData() const {
+    cout << "Total Sales: $" << totalSales << "\n";
+    for (const auto& transaction : salesRecords) {
+        cout << "Transaction ID: " << transaction.getProductID()
+            << ", Amount Paid: $" << transaction.getAmountPaid()
+            << ", Status: " << transaction.getStatus() << "\n";
+    }
+}
